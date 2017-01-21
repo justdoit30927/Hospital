cf1c6a9158c08f5efffd7913a3a9a89513ee17dc
justdoit30927 <justdoit30927@gmail.com>
2017-01-21 17:01:27 +0800

    increase.aspx.vb

---
+++ increase.aspx.vb
@@ -0,0 +1,12 @@
+
+Partial Class increase
+    Inherits System.Web.UI.Page
+
+    Private Sub increase_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+
+        Label1.Text = "123"
+        TextBox1.Text = "123"
+
+    End Sub
+End Class
